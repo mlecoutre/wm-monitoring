0dbe4b34f43eff00c5ba97ddf54fe2fe30cee411
e010925 <matthieu.lecoutre@arcelormittal.com>
2016-12-21 11:21:04 +0100

    Add event management between appCompoenent and Flow Selector panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,26 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     console.log("init APP!");
+  } 
+
+/**************
+ *  Retrieve events from Flow Selector panel
+ **************/ 
+
+  onPlatformName(platformName: string){
+    console.log("AppComponent: get event onPlatformName :"+platformName);
+  }
+
+  onDomainName(domainName: string){
+    console.log("AppComponent: get event onDomainName :"+domainName);
+  }
+
+  onPeName(peName: string){
+    console.log("AppComponent: get event onPeName :"+peName);
+  }
+
+  onFlowName(flowName: string){
+    console.log("AppComponent: get event onFlowName :"+flowName);
   }
 
 }
--- src/app/flow-selector-component/flow-selector-component.component.ts
+++ src/app/flow-selector-component/flow-selector-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  EventEmitter, Input, Output} from '@angular/core';
 import {PlatformsService} from '../services/platforms.service'
 import * as model from '../model/model'
 
@@ -11,21 +11,26 @@ export class FlowSelectorComponentComponent implements OnInit {
 
   platforms: model.Platform[];
 
+
   selectedPlatform: model.Platform ;
   selectedDomain: model.Domain;
   selectedPE: model.Pe;
   selectedMd: model.Flux;
 
+  /**
+   * Emission d'event vers le composant parent (AppComponent) 
+   */
+  @Output() onPlatformName = new EventEmitter<string>();
+  @Output() onDomainName = new EventEmitter<string>();
+  @Output() onPeName = new EventEmitter<string>();
+  @Output() onFlowName = new EventEmitter<string>();
 
-  constructor(
-    private platformsService: PlatformsService
-  ) {
+  constructor( private platformsService: PlatformsService ) {
     console.log('FlowSelectorComponentComponent construct');
-
    }
 
    /**
-    * Call platformsService
+    * Call platformsService during the init phase.
     */
   ngOnInit() {
     console.log("Init Flow-selector-component");
@@ -35,21 +40,25 @@ export class FlowSelectorComponentComponent implements OnInit {
 
   onPlatformSelected(pf: model.Platform){
     console.log("selectedPlaform: "+ pf.name);
+    this.onPlatformName.emit(pf.name);
     this.selectedPlatform = pf;
   }
 
   onDomainSelected(d: model.Domain){
     console.log("selectedDomain: "+ d.name);
+    this.onDomainName.emit(d.name);
     this.selectedDomain = d;
   }
 
   onPeSelected(p: model.Pe){
     console.log("selectedPE: "+ p.name);
+    this.onPeName.emit(p.name);
     this.selectedPE = p;
   }
 
   onFlowSelected(f: model.Flux){
     console.log("selectedFlux: "+ f.name);
+    this.onFlowName.emit(f.name);
     this.selectedMd = f;
   }
 
